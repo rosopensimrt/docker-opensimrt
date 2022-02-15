416428f29dc131037e0efff12bef82a395a08dd7
fbk <frekle@kth.se>
2022-02-15 18:09:36 +0100

    simplify windows

--- run_docker_image.sh
+++ run_docker_image.sh
@@ -18,9 +18,6 @@ elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
 		mysablehats/opensim-rt:latest
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
     # Do something under 64 bits Windows NT platform
-	/C/Program\ Files\ \(x86\)/Xming/Xming.exe :0 -multiwindow -silent-dup-error &
-    	IP=$( grep -E "Xdm" /c/Users/frekle/AppData/Local/Temp/Xming.0.log | grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])')
-    	export DISPLAY=$IP:0.0
 	winpty docker run --rm -it -p 8080:8080/udp -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix mysablehats/opensim-rt:devel
 
 
