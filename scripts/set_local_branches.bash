9fe7d8a0e2c34c7e1fa578eb90083d2011e64f3e
frederico-klein <frekle@kth.se>
2023-06-30 13:15:43 +0200

    .

--- scripts/set_local_branches.bash
+++ scripts/set_local_branches.bash
@@ -3,7 +3,10 @@ git config --global user.name "frekle"
 
 cd /catkin_opensim/src/opensimrt_core
 git remote add local /catkin_ws/OpenSimRT.git 
+git pull local
 cd /catkin_opensim/src/opensimrt_bridge
 git remote add local /catkin_ws/opensimrt_bridge.git 
+git pull local
 cd /catkin_opensim/src/opensimrt_msgs
 git remote add local /catkin_ws/opensimrt_msgs.git
+git pull local
